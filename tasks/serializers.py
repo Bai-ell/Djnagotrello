25e11b59802e52d47fab167e5fd743a2ac89c738
Baiel <apple@Baiel.local>
2025-03-25 02:58:28 +0600

    api done

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -7,16 +7,35 @@ from .models import User, Card, Task
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = '__all__' 
+        fields = '__all__'
 
 
-class CardSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Card
-        fields = '__all__'  
 
 
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
-        fields = '__all__'  
+        fields = '__all__'
+
+
+
+
+class CardSerializer(serializers.ModelSerializer):
+    tasks = TaskSerializer(many=True, read_only=True)
+    
+    class Meta:
+        model = Card
+        fields = '__all__'
+        extra_kwargs = {
+            'user': {'read_only': True} 
+        }
+
+
+
+class TaskUpdateSerializer(serializers.Serializer):
+    task_id = serializers.IntegerField()
+    done = serializers.BooleanField()
+
+
+class TaskDeleteSerializer(serializers.Serializer):
+    task_id = serializers.IntegerField()
