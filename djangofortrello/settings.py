e9abcdd56a2de46da1b2a5bf4749ccc563f74c41
Baii-ell <bosmn.bosmn.bosmn@gmail.com>
2025-03-25 23:54:06 +0600

    change bugs

--- djangofortrello/settings.py
+++ djangofortrello/settings.py
@@ -29,7 +29,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 DEBUG = os.environ.get('DEBUG', False) == True
-SECRET_KEY = env('SECRET_KEY') 
+SECRET_KEY = os.environ.get('SECRET_KEY') 
 
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(' ')
